88ae83f255d3c31d319693b48d25317d66f90e7c
jhuffman083 <jhuffman083@gmail.com>
2023-04-23 14:09:26 -0400

    chaned volume for sound effects

--- sound_effects.py
+++ sound_effects.py
@@ -3,6 +3,7 @@ import pygame
 pygame.mixer.init()
 #set a sounnd for shooting to a vairable so we can work with it in alien invasion
 bullet_sound = pygame.mixer.Sound('sounds/phasers3.wav')
+bullet_sound.set_volume(0.3)
 #set a sounnd for killing an alien to a vairable so we can work with it in alien invasion
 explosion_sound = pygame.mixer.Sound('sounds/explosion.mp3')
 #set a sound for ehen the ship is hit and the player dies
